b6ca3cde0922da4cfff7f9ae5735451e82394eb4
hoodsey <kabakova.nas@gmail.com>
2023-10-22 07:41:06 +0300

    Add test for authorization

--- src/test/java/MainActivity.kt
+++ src/test/java/MainActivity.kt
@@ -32,7 +32,7 @@ class MainActivity {
 
         androidDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(1))
 
-        // обработка ошибок при прокликиваинии элементов
+        // обработка ошибок при прокликиваинии элементов в сценарии атворизации по номеру +7 951 055 65 86
         try {
             val element1 = androidDriver.findElement(AppiumBy.xpath("//android.view.View[@content-desc=\"Русский\"]"))
             element1.click()
@@ -48,19 +48,41 @@ class MainActivity {
             val element4 = androidDriver.findElement(AppiumBy.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android.widget.ImageView[3]"))
             element4.click()
 
+            val element6 = androidDriver.findElement(AppiumBy.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android.widget.ImageView[1]"))
+            element6.click()
+
+            val element7 = androidDriver.findElement(AppiumBy.accessibilityId("Войти"))
+            element7.click()
+
+            val element8 = androidDriver.findElement(AppiumBy.xpath("//android.view.View[@content-desc=\"Укажите телефон\n" + "На него отправим код подтверждения\"]/android.widget.EditText"))
+            element8.sendKeys("9510556586")
+
+            val element9 = androidDriver.findElement(AppiumBy.accessibilityId("Получить код"))
+            element9.click()
+
+            TimeUnit.SECONDS.sleep(5)
+            val element10 = androidDriver.findElement(AppiumBy.xpath("//*[starts-with(@content-desc,'Введите код из смс')]"))
+
+
+            val fullXPath = element10.getAttribute("content-desc")
+            //println("Вывод" + fullXPath)
+
+            val substringAfterLast = fullXPath.substringAfterLast("смс\n")
+
+            val element11 = androidDriver.findElement(AppiumBy.className("android.widget.EditText"))
+            element11.sendKeys(substringAfterLast)
+
+            // тест с ошибкой при поиске по resource-id
             val element5 = androidDriver.findElement(AppiumBy.id("Fail resource-id"))
             element5.click()
+
 
         } catch (e: org.openqa.selenium.NoSuchElementException) {
             e.printStackTrace() // распечатываем ошибку в консоль
             println("Мы поймали ошибку, и теперь тест не упадет")
 
         }
-
-        /* val element4 =  androidDriver.findElement(AppiumBy.accessibilityId("За столик"))
-        element4 = sendKeys("текст")
-         авторизация по коду выделить кусок от и до
-
+        /*
         val text = androidDriver.pageSource()
         */
         TimeUnit.SECONDS.sleep(10)
