0e54aa102407133b69face1463e8fe48f1f37c64
hoodsey <kabakova.nas@gmail.com>
2023-10-26 17:59:53 +0300

    change object to AurhorizationApp

--- src/test/java/general_cases_for_tests/AuthorizationScenarios.kt
+++ src/test/java/general_cases_for_tests/AuthorizationScenarios.kt
@@ -3,9 +3,12 @@ package general_cases_for_tests
 import LocatorType
 import TestFunctions.checkAvailableElement
 import TestFunctions.clickToElement
-import general_cases_for_tests.AuthorizationApp.authorizationApp
+import TestFunctions.getAttribute
+import TestFunctions.sendText
+import screens.Authorization
 import screens.MenuApps.selectCatalogButton
 import screens.MenuApps.selectProfileButton
+import screens.Profile
 import screens.Profile.quietButton
 
 
@@ -16,6 +19,7 @@ object AuthorizationScenarios {
         clickToElement(selectProfileButton.androidXPath, LocatorType.XPATH)
 
         var userIsAuthorization: Boolean
+
         try {
             userIsAuthorization = checkAvailableElement(quietButton.androidAccessibilityId, LocatorType.ACCESSIBILITY_ID)
         } catch (e: org.openqa.selenium.NoSuchElementException) {
@@ -23,7 +27,9 @@ object AuthorizationScenarios {
         }
         when {
             // нужна авторизация и есть кнопка выйти в профиле(авторизован)
-            needAuthorizationUser && userIsAuthorization -> {clickToElement(selectCatalogButton.androidXPath, LocatorType.XPATH)}
+            needAuthorizationUser && userIsAuthorization -> {
+                clickToElement(selectCatalogButton.androidXPath, LocatorType.XPATH)
+            }
             //нужна авторизация и кнопке выйти нет(неавторизован)  -> авторизация + переход к каталогу
             needAuthorizationUser && !userIsAuthorization -> {
                 authorizationApp( "9510556586")
@@ -35,7 +41,17 @@ object AuthorizationScenarios {
                 clickToElement(selectCatalogButton.androidXPath, LocatorType.XPATH)
             }
             //не нужна авторизация и не авторизован
-            !needAuthorizationUser && !userIsAuthorization ->{clickToElement(selectCatalogButton.androidXPath, LocatorType.XPATH)}
+            !needAuthorizationUser && !userIsAuthorization ->{
+                clickToElement(selectCatalogButton.androidXPath, LocatorType.XPATH)
+            }
         }
+    }
+    fun  authorizationApp(telephone: String){
+        clickToElement(Profile.authorizationButton.androidAccessibilityId, LocatorType.ACCESSIBILITY_ID)
+        sendText(Authorization.selectTelephone.androidXPath, LocatorType.XPATH,telephone)
+        clickToElement(Authorization.codeButton.androidAccessibilityId, LocatorType.ACCESSIBILITY_ID)
+        val fullXPath = getAttribute(Authorization.selectCodeAuthorization.androidXPath, LocatorType.XPATH, "content-desc").substringAfterLast("смс\n")
+
+        sendText(Authorization.selectCodeInsert.className, LocatorType.CLASS_NAME,fullXPath)
     }
 }
